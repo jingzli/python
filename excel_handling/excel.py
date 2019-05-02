e080407dc94f627db0808756c1d15facc1b14511
Jing Li <25649299+jingzlo@user.noreply.github.com>
2019-05-01 23:02:57 -0700

    panda

--- excel_handling/excel.py
+++ excel_handling/excel.py
@@ -20,3 +20,17 @@ print( 'Cell ' + c.coordinate + ' is ' + c.value)
 # 'Cell B1 is Apples'
 print(sheet['C1'].value)
 # 73
+
+
+
+>>> from openpyxl import Workbook
+>>> wb = Workbook(write_only = True)
+>>> ws = wb.create_sheet()
+>>> from openpyxl.cell import WriteOnlyCell
+>>> from openpyxl.comments import Comment
+>>> from openpyxl.styles import Font
+>>> cell = WriteOnlyCell(ws, value="hello world")
+>>> cell.font = Font(name='Courier', size=36)
+>>> cell.comment = Comment(text="A comment", author="Author's Name")
+>>> ws.append([cell, 3.14, None])
+>>> wb.save('write_only_file.xlsx')
